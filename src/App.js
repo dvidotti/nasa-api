0a9ab87e6b8e9fb42e0d6109254c1c13da922e11
dvidotti <danilovidotti@gmail>
2019-12-20 13:47:27 +0000

    refactor:create Home CSS, still needs ajustments

--- src/App.js
+++ src/App.js
@@ -4,15 +4,28 @@ import './App.css';
 import Home from './Home/Home';
 import ApolloFeed from './ApolloFeed/ApolloFeed'
 import Details from './ApolloFeed/Details/Details';
-import Rovers from './Rovers/Rovers'
+import Rovers from './Rovers/Rovers';
+import { withRouter} from 'react-router-dom'
 
-const App = () => {
+const App = (props) => {
+  console.log(props.location);
+
+  const navColor = () => {
+    if (props.location.pathname === '/') {
+      return 'first-child-selected'
+    } if (props.location.pathname === '/apollo') {
+      return 'second-child-selected'
+    } if (props.location.pathname === '/rovers') {
+      return 'third-child-selected'
+    }
+  }
+
   return (
     <div className="App">
-      <nav>
-        <a href="/">HOME</a>
-        <a href="/apollo">APOLLO</a>
-        <a href="/rovers">ROVER</a>
+      <nav className={`navbar ${navColor()}`}>
+        <a className="nav-item" href="/">HOME</a>
+        <a className="nav-item" href="/apollo">APOLLO</a>
+        <a className="nav-item" href="/rovers">ROVER</a>
       </nav>
       <Switch>  
         <Route exact path='/' component={Home} />
@@ -24,4 +37,4 @@ const App = () => {
   );
 }
 
-export default App;
+export default withRouter(App);
--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
-import axios from 'axios'
+import axios from 'axios';
+import './Home.css'
 
 const Home = () => {
   const [pic, handlePic] = useState('');
@@ -9,7 +10,7 @@ const Home = () => {
   const today = new Date()
   const day = today.toString().split(' ')[2];
   const year = today.toString().split(' ')[3];
-  console.log(process.env.REACT_APP_NASA_API_KEY)
+  console.log(pic)
 
   const formatDate = () => {
     const monthText = today.toString().split(' ')[1];
@@ -29,31 +30,61 @@ const Home = () => {
     handleDate(formatDate())
   }
 
+
+  // useEffect(() => {
+  //   if (pic === '') {
+  //     moveDayBack();
+  //   }
+  // })
+
   useEffect(() => {
     const  getPic = async () => {
       try {
         const day_pic_href = await axios.get(`https://api.nasa.gov/planetary/apod?date=${formatDate() + "&"}api_key=${process.env.REACT_APP_NASA_API_KEY}`);
         handlePic(day_pic_href);
       }
-      catch(error) { console.log(error) }
+      catch(error) { 
+        console.log(error);
+        //setDayBeforeMessage();
+        moveDayBack() ;
+      }
     } ;
     getPic();
   },[date])
 
   return (
-    <div className="Home">
+    <section className="container-home">
+
       { pic !== '' ? 
-      <div>
-      <h1>{pic.data.title}</h1>
-      <p>Copyright: {pic.data.copyright}</p>
-      <img src={pic.data.url}></img>
-      <p>{pic.data.explanation}</p>
+      <div className="container-home-info">
+
+        <div className="picture-top-legend float-right">
+          <h1 id="picture-title-right-position">{pic.data.title}</h1>
+          <div class="legend-right-block">
+            <button id="move-back" onClick={moveDayBack}>{"<"}</button>
+            {moveDays < 0 &&
+            <button id="move-foward" onClick={moveDayAfter}>{">"}</button>
+            }
+            <h1 id="date-info">{pic.data.date}</h1>
+          </div>
+        </div>
+        
+        <div className="big-picture-box float-right">
+          <img src={pic.data.url}></img>
+        </div>
+
+      
+        <div className="block-absolute-position">
+          <div className="inner-block-absolute-position" >
+            <div className="block-info">
+              <p className="text-inside-block">{pic.data.explanation}</p>
+              <p>Copyright: {pic.data.copyright}</p>
+            </div>
+          </div>
+        </div> 
       </div> : null }
-      <button onClick={moveDayBack}>Day Before Picture</button>
-      {moveDays < 0 &&
-      <button onClick={moveDayAfter}>Day After</button>
-      }
-    </div>
+     
+    </section>
   );
 }
 
